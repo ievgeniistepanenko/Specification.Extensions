abd516348c7cf0a35be1144a334996b632219424
Ievgenii Stepanenko <iev@conexio.dk>
2018-11-23 17:29:55 +0100

    Queryable extension for ISpecification

---
+++ src/Specification.Extensions/QueryableExtension.cs
@@ -0,0 +1,14 @@
+﻿using Specification.Interfaces;
+using System.Linq;
+
+namespace Specification.Extensions
+{
+    public static class QueryableExtension
+    {
+        public static IQueryable<T> Where<T>(this IQueryable<T> source, ISpecification<T> spec)
+        {
+            return source.Where(spec.ToExpression());
+        }
+    }
+
+}
