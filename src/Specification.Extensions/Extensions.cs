303c77853206fb96c1223776c5bd08351f531523
Ievgenii Stepanenko <iev@conexio.dk>
2018-11-23 17:34:14 +0100

    Add IEnumarable extension

--- src/Specification.Extensions/Extensions.cs
+++ src/Specification.Extensions/Extensions.cs
@@ -1,13 +1,19 @@
 ﻿using Specification.Interfaces;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Specification.Extensions
 {
-    public static class QueryableExtension
+    public static class Extensions
     {
         public static IQueryable<T> Where<T>(this IQueryable<T> source, ISpecification<T> spec)
         {
             return source.Where(spec.ToExpression());
+        }
+
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> source, ISpecification<T> spec)
+        {
+            return source.Where(spec.IsSatisfiedBy);
         }
     }
 
